371db9b9293bb6c3672b8cd0a2d89b218729f5be
zainabjawaid <banozainie24@gmail.com>
2025-12-12 22:05:00 +0500

    OTP Verification

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-import { getAuth, signInWithPhoneNumber, RecaptchaVerifier } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
-
+l
 const firebaseConfig = {
     apiKey: "AIzaSyCZX1DNbvpc63B_NbWbixVcEB_cu5I4rNk",
     authDomain: "phone-number-validation-7b7cb.firebaseapp.com",
@@ -19,19 +19,19 @@ const auth = getAuth(app);
 const numberInput = document.getElementById("number").value;
 const sendbtn = document.getElementById("send");
 sendbtn.addEventListener("click", () => {
-    const appVerifier = new RecaptchaVerifier(auth, 'sendOTP',{
-        'size' : 'invisible'
+    const appVerifier = new RecaptchaVerifier(auth, 'sendOTP', {
+        'size': 'invisible'
     });
     signInWithPhoneNumber(auth, numberInput, appVerifier)
-    .then((result) => {
-        window.result = result;
-    })
-    .catch((err) => {
-         swal({
-             title: "Error",
-             text: "PLease give a valid phone number, Try again ❌",
-             icon: "error",
-             button: "Sure"
-})
-    })
+        .then((result) => {
+            window.result = result;
+        })
+        .catch((err) => {
+            swal({
+                title: "Error",
+                text: "PLease give a valid phone number, Try again ❌",
+                icon: "error",
+                button: "Sure"
+            })
+        })
 });
